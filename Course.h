59792bc63e01159c76574ab1478509527e3adeaf
Youssof mostafa <163543255+Youssof-M@users.noreply.github.com>
2025-11-11 10:33:50 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -15,10 +15,10 @@ private:
     string courseName;
     int maxStudents;
     int currentStudents;
-    Student* students; // dynamically allocated array of students
+    Student* students; 
 
 public:
-    // Constructors
+  
     Course();
     Course(string courseCode, string courseName, int maxStudents, Student* initialStudents = nullptr, int currentStudents = 0);
 
